42147f5921c74b176a5329b429e13c1a01f5ac68
Gregg <opstyix@gmail.com>
2023-11-17 00:30:00 -0500

    chore: cleanup

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ from .operators import scatter
 
 from .operators.mat_nodeautoname import register_mat_nodeautoname, unregister_mat_nodeautoname
 from .operators.original import register_original, unregister_original
-#from .operators.scatter import register_scatter, unregister_scatter
+# from .operators.scatter import register_scatter, unregister_scatter
 from .operators.kitbash3d import register_kitbash3d, unregister_kitbash3d
 from .operators.vp_emissive import register_vp_emissive, unregister_vp_emissive
 from .operators.outliner import register_outliner, unregister_outliner
@@ -53,7 +53,7 @@ def register():
 def unregister():      
     unregister_mat_nodeautoname()
     unregister_original()
-    #unregister_scatter()
+    # unregister_scatter()
     scatter.unregister()
     unregister_kitbash3d()
     unregister_vp_emissive()
